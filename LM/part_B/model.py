2c582933489b6fd304fac1629961228ba96e01e7
Moska002 <ale.moska002@gmail.com>
2025-03-25 14:43:30 +0100

    ADD Base LSTM model

---
+++ LM/part_B/model.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+
+class LM_LSTM(nn.Module):
+    def __init__(self, emb_size, hidden_size, output_size, pad_index=0, n_layers=1):
+        super(LM_LSTM, self).__init__()
+        # Token ids to vectors, we will better see this in the next lab 
+        self.embedding = nn.Embedding(output_size, emb_size, padding_idx=pad_index)
+        # Pytorch's LSTM layer
+        self.lstm = nn.LSTM(emb_size, hidden_size, n_layers, bidirectional=False, batch_first=True)    
+        self.pad_token = pad_index
+        # Linear layer to project the hidden layer to our output space 
+        self.output = nn.Linear(hidden_size, output_size)
+        
+    def forward(self, input_sequence):
+        emb = self.embedding(input_sequence)
+        lstm_out, _  = self.lstm(emb)
+        output = self.output(lstm_out).permute(0,2,1)
+        return output 
