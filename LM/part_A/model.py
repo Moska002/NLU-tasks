4fdbbc9dc277739e1cd3e9a234c9e2a8cd8b2e30
Moska002 <ale.moska002@gmail.com>
2025-03-25 10:45:59 +0100

    ADD dropout model

--- LM/part_A/model.py
+++ LM/part_A/model.py
@@ -2,19 +2,42 @@ import torch
 from torch import nn
 
 class LM_LSTM(nn.Module):
-    def __init__(self, emb_size, hidden_size, output_size, pad_index=0, out_dropout=0.1,
-                 emb_dropout=0.1, n_layers=1):
+    def __init__(self, emb_size, hidden_size, output_size, pad_index=0, n_layers=1):
         super(LM_LSTM, self).__init__()
         # Token ids to vectors, we will better see this in the next lab 
         self.embedding = nn.Embedding(output_size, emb_size, padding_idx=pad_index)
         # Pytorch's LSTM layer
-        self.rnn = nn.LSTM(emb_size, hidden_size, n_layers, bidirectional=False, batch_first=True)    
+        self.lstm = nn.LSTM(emb_size, hidden_size, n_layers, bidirectional=False, batch_first=True)    
         self.pad_token = pad_index
         # Linear layer to project the hidden layer to our output space 
         self.output = nn.Linear(hidden_size, output_size)
         
     def forward(self, input_sequence):
         emb = self.embedding(input_sequence)
-        rnn_out, _  = self.rnn(emb)
-        output = self.output(rnn_out).permute(0,2,1)
+        lstm_out, _  = self.lstm(emb)
+        output = self.output(lstm_out).permute(0,2,1)
+        return output 
+    
+class LM_LSTM_drop(nn.Module):
+    def __init__(self, emb_size, hidden_size, output_size, pad_index=0, out_dropout=0.1,
+                 emb_dropout=0.1, n_layers=1):
+        super(LM_LSTM_drop, self).__init__()
+        # Token ids to vectors, we will better see this in the next lab 
+        self.embedding = nn.Embedding(output_size, emb_size, padding_idx=pad_index)
+        # First dropout layer
+        self.emb_drop = nn.Dropout(p=emb_dropout)
+        # Pytorch's LSTM layer
+        self.lstm = nn.LSTM(emb_size, hidden_size, n_layers, bidirectional=False, batch_first=True)    
+        self.pad_token = pad_index
+        # Second dropout layer
+        self.out_drop = nn.Dropout(p=out_dropout)
+        # Linear layer to project the hidden layer to our output space 
+        self.output = nn.Linear(hidden_size, output_size)
+        
+    def forward(self, input_sequence):
+        emb = self.embedding(input_sequence)
+        emb_drop = self.emb_drop(emb)
+        lstm_out, _  = self.lstm(emb_drop)
+        out_drop = self.out_drop(lstm_out)
+        output = self.output(out_drop).permute(0,2,1)
         return output 
